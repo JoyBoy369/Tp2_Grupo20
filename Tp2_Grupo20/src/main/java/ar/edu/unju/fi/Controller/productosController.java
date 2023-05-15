311ff5581d876a91a06a65d321b97ca9bed25d16
Fernandez Francisco Jesus Maria <panhofer1122@gmail.com>
2023-05-15 19:15:36 -0300

    02: Creación de la capa Model y las clases @controller

---
+++ Tp2_Grupo20/src/main/java/ar/edu/unju/fi/Controller/productosController.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.Controller;
+
+public class productosController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Tp2_Grupo20/src/main/java/ar/edu/unju/fi/model/servicio.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.model;
+
+public class servicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
