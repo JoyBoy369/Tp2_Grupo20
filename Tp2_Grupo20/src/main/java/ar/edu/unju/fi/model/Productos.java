a591dfeebba9b1d50b7e620bd122e4c41593df66
Juan Ignacio Pantaleon <juanp@DESKTOP-NBI27SV>
2023-06-04 15:46:52 -0300

    Productos Validacion y @component

--- Tp2_Grupo20/src/main/java/ar/edu/unju/fi/model/Productos.java
+++ Tp2_Grupo20/src/main/java/ar/edu/unju/fi/model/Productos.java
@@ -1,12 +1,36 @@
 package ar.edu.unju.fi.model;
 
-public class Productos {
-			
-		private String nombre,categoria;
-		private int codigo;
-		private double precio,descuento;
-		
+import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+@Component
+public class Productos {
+		
+	
+		@NotEmpty(message = "El Nombre no puede estar vacio")
+		private String nombre;
+		
+		@NotBlank(message = "La Categoria no puede estar vacia")
+		private String categoria;		
+		
+		@Positive(message="El codigo no pude ser negativo")
+		private int codigo;		
+		
+		@Positive(message="El precio no pude ser negativo ni 0")
+		private double precio;		
+		
+		@Max(value=50,message="El descuento debe ser menor o igual de 50%")
+		@Min(value=0,message="El descuento debe ser positivo")
+		private double descuento;
+		
 		public Productos() {
 			// TODO Auto-generated constructor stub
 		}
