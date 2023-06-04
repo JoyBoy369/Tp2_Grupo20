a149322863fc73849b750ec13f8e044988cc55c9
Acho Lucas Santiago <acholucas136@gmail.com>
2023-06-03 23:13:40 -0300

    [TP5] Sucursales Validacion y @Component

--- Tp2_Grupo20/src/main/java/ar/edu/unju/fi/model/Sucursales.java
+++ Tp2_Grupo20/src/main/java/ar/edu/unju/fi/model/Sucursales.java
@@ -2,13 +2,35 @@ package ar.edu.unju.fi.model;
 
 import java.time.LocalDate;
 
-public class Sucursales {
+import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+
+@Component
+public class Sucursales {
+	@NotEmpty(message = "El Nombre no puede estar vacio")
 	private String nombre;
+	
+	@Size(min=5,max=150,message = "Ingrese una direccion completa")
 	private String direccion;
+	
+	@NotBlank(message = "Seleccione una Provincia")
 	private String provincia;
+	
+	@PastOrPresent(message = "Ingrese la Fecha cuando se inaguro la Sucursal (No puede ser mayor que hoy)")
+	@NotNull(message = "No puede estar nulo la fecha")
 	private LocalDate fecha;
+	
+	@Email(message = "Ingrese un correo valido")
+	@NotEmpty(message = "No puede esatr vacio el correo")
 	private String correo;
+	
+	@Size(min=10,max=18,message = "Ingrese un telefono valido Ej:3880934183")
 	private String telefono;
 
 	public Sucursales() {
