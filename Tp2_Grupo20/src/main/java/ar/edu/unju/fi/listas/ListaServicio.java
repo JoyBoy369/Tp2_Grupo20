f9a7a5291a55e0347b32e2629a0d42620b96a13a
AchoLucasSantiago <acholucas136@gmail.com>
2023-06-04 23:17:25 -0300

    Servicio paseos Validacion y @Component

--- Tp2_Grupo20/src/main/java/ar/edu/unju/fi/listas/ListaServicio.java
+++ Tp2_Grupo20/src/main/java/ar/edu/unju/fi/listas/ListaServicio.java
@@ -4,17 +4,20 @@ package ar.edu.unju.fi.listas;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import ar.edu.unju.fi.model.Servicios;
 
+@Component
 public class ListaServicio {
 	private List<Servicios> servicio;
 	
 	public ListaServicio() {
 		servicio = new ArrayList<Servicios>();
 		
-		servicio.add(new Servicios("0021","Carlos","09:20","12:00","---","---","10:30"));
-		servicio.add(new Servicios("0210","Nacho","19:30","09:00","---","---","14:00"));
-		servicio.add(new Servicios("3696","Melody","19:30","12:00","---","---","15:15"));
+		servicio.add(new Servicios("0021","Carlos","09:20","12:00","-----","-----","10:30"));
+		servicio.add(new Servicios("0210","Nacho","19:30","09:00","-----","-----","14:00"));
+		servicio.add(new Servicios("3696","Melody","19:30","12:00","-----","-----","15:15"));
 	}
 
 	public List<Servicios> getServicio() {
